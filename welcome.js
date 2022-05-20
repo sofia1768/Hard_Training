a434a4bea5c9d0507b30ddebc4c14ef30622979c
Sofia <sofia@dtonic.io>
2022-05-20 14:50:15 +0900

    test commit

---
+++ welcome.js
@@ -0,0 +1,19 @@
+function welcomeToVSCode() {
+  console.log(helloWorld());
+
+}
+
+function helloWorld() {
+  return 'hello world';
+}
+
+welcomeToVSCode();
+
+function addValue(a,b) {
+  var a = 1;
+  var b = 2;
+
+  return a+b;
+}
+
+console.logg(addValue());
